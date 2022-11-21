d4201c98617d81ff1ca67be4eb17f52db7374e6f
Kostiantyn <konstantinokhorzin@gmail.com>
2022-11-21 16:24:11 +0200

    responsive

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const refs = {
-  openModalBtn: document.querySelector("[data-open-modal]"),
-  closeModalBtn: document.querySelector("[data-close-modal]"),
-  backdrop: document.querySelector("[data-backdrop]"),
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  refs.backdrop.classList.toggle("backdrop--hidden");
+  refs.backdrop.classList.toggle('backdrop--hidden');
 }
+
+const menuBtnRef = document.querySelector('.header__btn-burger');
+// const mobileMenuRef = document.querySelector('.header__icon-burger');
+
+menuBtnRef.addEventListener('click', () => {
+  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+  menuBtnRef.classList.toggle('header__btn-burger--opened');
+  menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+  // mobileMenuRef.classList.toggle('is-open');
+});
